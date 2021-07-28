d82d0cfa544721a125d47c8ad04120c2e4e022d8
Ryan Epp <re17wj@brocku.ca>
2021-07-28 12:42:38 -0400

    Added field and file searching

--- DelGen.Check.py
+++ DelGen.Check.py
@@ -2,6 +2,7 @@ import os, json, re
 from packaging.version import Version
 
 home_dir = os.getcwd()
+filesHere = os.listdir()
 
 print("Working in " + home_dir)
 print("\nLoading manifest...")
@@ -21,7 +22,7 @@ try:
             print("Your \"guid\" is incorrectly formatted, please change it to follow the regex of:")
             print("\"a-z\", \"0-9\", \"_\", \".\"")
         else:
-            print("Verified")
+            print("Verified.")
     except:
         print("Failed to find \"guid\" field, make sure it is present in your manifest.")
 
@@ -30,7 +31,7 @@ try:
         testString = json_data["version"]
         try:
             Version(json_data["version"])
-            print("Verified")
+            print("Verified.")
         except:
             print("Version number invalid, make sure it follows the \"x.x.x\" semantic versioning.")
     except:
@@ -41,11 +42,92 @@ try:
         testString = json_data["require"]
         try:
             Version(json_data["require"])
-            print("Verified")
+            print("Verified.")
         except:
             print("Version number invalid, make sure it follows the \"x.x.x\" semantic versioning.")
     except:
         input("Failed to find \"require\" field, make sure it is present in your manifest.")
 
+    print("\nVerifying \"dependencies\"...")
+    try:
+        testDict = json_data["dependencies"]
+        
+        try:
+            if not isinstance(json_data["dependencies"], dict):
+                raise Exception("TypeError")
+            else:
+                print("Verified.")
+        except:
+            print("\"dependencies\" field is not a dictionary. Make ure it follows the following format:")
+            print("{ \"dependency guid\" : \"x.x.x\" } with commas seperating each dependency")
+    except:
+        print("\"dependencies\" field not found.")
+
+    print("\nVerifying \"name\"...")
+    try:
+        testString = json_data["name"]
+        print("Verified.")
+    except:
+        print("Failed to find \"name\" field, make sure it is present in your manifest.")
+
+    print("\nVerifying \"description\"...")
+    try:
+        testString = json_data["description"]
+        print("Verified.")
+    except:
+        print("Failed to find \"description\" field, make sure it is present in your manifest.")
+
+    print("\nVerifying \"authors\"...")
+    try:
+        if not isinstance(json_data["authors"], list):
+            raise Exception("TypeError")
+        else:
+            print("Verified.")
+    except:
+        print("Failed to find or confirm validity \"authors\" field, make sure it is present in your manifest and follows correct format:")
+        print("Is a list (\'[\' and \']\' encapsulate your object), and dependency strings are seperated by commas.")
+
+    print("\nVerifying \"assets\"...")
+    try:
+        testDict = json_data["assets"]
+        print("\"assets\" field present.")
+        print("\tVerifying sub-fields in \"assets\"...")
+        try:
+            if not isinstance(json_data["assets"], dict):
+                raise Exception("TypeError")
+            else:
+                def checkAssets(fieldname):
+                    try:
+                        json_data["assets"][fieldname]
+                        try:
+                            if not isinstance(json_data["assets"][fieldname], dict):
+                                raise Exception("TypeError")
+                            print("\t" + fieldname +" asset field verified, checking associated files...")
+
+                            for asset in json_data["assets"][fieldname]:
+                                print("\t\tChecking " + str(asset) + "...")
+                                found = False
+                                for file in filesHere:
+                                    if file == asset:
+                                        print("\t\tVerified asset")
+                                        found = True
+                                        break
+                                if not found:
+                                    print("Failed to find asset, make sure it is named correctly.")
+                        except:
+                            print(fieldname + " assets are not in dictionary format.")
+                    except:
+                        print("\tMissing " + fieldname + " assets field. (This is not an error)")
+
+                checkAssets("runtime")
+                checkAssets("setup")
+                checkAssets("patcher")
+        except:
+            print("\"assets\" field is not a dictionary. Make ure it follows the following format:")
+            print("Has a loading stage with { and }.")
+    except:
+        print("Failed to find \"assets\" dictionary.")
 except:
-    input("Failed to find file called \"manifest.json\". Please make sure I have read permissions.")
+    print("Failed to find file called \"manifest.json\". Please make sure I have read permissions.")
+
+input("\nFinished check, please check errors if present.")
--- README.md
+++ README.md
@@ -8,6 +8,17 @@ This includes that all fields are present, and that:
   - Makes sure that there are no capitals or spaces
 - "version" and "require"
   - Follow correct versioning
+- "dependencies"
+  - Is a dict
 - "authors"
   - Is a list
-- 
+- "assets"
+  - Is a dict
+  - Has a loading phase
+    - 
+
+## Installation/Usage
+
+1. Download `DelGen.Check.exe` from the [Releases](https://github.com/nayr31/DelGen.Check/releases) page.
+2. Place it in a folder next to your files you wish to check. Make sure that the file is not zipped. If you do not have read permissions inside the folder, move the files somewhere else, or run `DelGen.Check.exe` as administrator.
+3. Run the program and follow the terminal prompts.
